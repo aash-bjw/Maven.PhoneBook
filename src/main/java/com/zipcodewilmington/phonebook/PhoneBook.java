558f0d2449a386687f4b501d8d0cffec60a7e113
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-11-13 21:49:22 -0500

    remove & reverse tests failing

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,67 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private Map<String,List<String>> phoneBook;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.phoneBook = map;
     }
 
     public PhoneBook() {
-        this(null);
+
+        this.phoneBook = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        ArrayList<String> pNumbers = new ArrayList<>();
+        pNumbers.add(phoneNumber);
+        phoneBook.put(name, pNumbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        phoneBook.put(name, Arrays.asList(phoneNumbers));
     }
 
     public void remove(String name) {
+        if(phoneBook.containsKey(name))
+        phoneBook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+
+        return phoneBook.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+
+        return phoneBook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        String result = "";
+
+        for (Map.Entry<String, List<String>> entry : phoneBook.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                result = entry.getKey();
+            }
+        }
+        return result;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+
+        ArrayList<String> contactList = new ArrayList<>(phoneBook.keySet());
+        return contactList;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+
+        return phoneBook;
     }
 }
